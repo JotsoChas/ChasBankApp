2bbd79e83c7960f5b175ab0e9b2db9d661c9513f
JocoChas <joco.borghol@chasacademy.se>
2025-09-30 16:25:54 +0200

    Add project files.

---
+++ ChasBankApp/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace ChasBankApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
